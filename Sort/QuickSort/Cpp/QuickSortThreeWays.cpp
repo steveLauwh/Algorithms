ca09620ab347347f7c52d29eba38e0381e6de63d
Steve Lau <liuwenhong_steve@163.com>
2017-07-28 08:38:34 +0800

    Create QuickSortThreeWays.cpp

---
+++ Sort/QuickSort/Cpp/QuickSortThreeWays.cpp
@@ -0,0 +1,19 @@
+/* 三路快排：对于包含大量重复数据的数组，使用三路快排有巨大的优势
+ * 对于一般性的随机数组和近乎有序的数组, 三路快排的效率虽然不是最优的, 但是是在非常可以接受的范围里
+ * 三路快排是 Java 语言中默认的排序算法
+ */
+ 
+ #include <iostream>
+ 
+ using namespace std;
+ 
+ template <typename T>
+ void quickSortThreeWays(T arr[], int low, int high)
+ {
+ 
+ }
+ 
+ int main()
+ {
+    return 0;
+ }
