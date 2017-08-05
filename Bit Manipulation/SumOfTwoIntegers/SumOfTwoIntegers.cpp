861755a60fc6537e324454ee2e3ebe41906ad233
Steve Lau <liuwenhong_steve@163.com>
2017-08-05 16:38:51 +0800

    Create SumOfTwoIntegers.cpp

---
+++ Bit Manipulation/SumOfTwoIntegers/SumOfTwoIntegers.cpp
@@ -0,0 +1,10 @@
+/**
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ */
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+    
+    }
+};
