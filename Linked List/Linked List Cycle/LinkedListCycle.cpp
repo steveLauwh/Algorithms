b2aa83b26404fc83a80cdb6f1c065b3eda0c14d3
Steve Lau <liuwenhong_steve@163.com>
2017-08-05 16:18:50 +0800

    判断单链表是否存在环以及求出环的入口点

--- Linked List/Linked List Cycle/LinkedListCycle.cpp
+++ Linked List/Linked List Cycle/LinkedListCycle.cpp
@@ -1,8 +1,9 @@
 /**
  * Given a linked list, determine if it has a cycle in it.
+ * return the node where the cycle begins. If there is no cycle, return NULL.
  * Follow up:
  * Can you solve it without using extra space?
- * 判断单链表是否存在环以及求出环的入口点
+ * 
  */
  
 /**
@@ -17,6 +18,55 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-      
+        if (!head) {
+            return false;
+        }
+     
+        ListNode *slow = head;
+        ListNode *fast = head->next;  
+     
+        while (fast && fast->next) {
+            if (slow == fast) {
+                return true;
+            }
+         
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return false;
+    }
+ 
+    ListNode* detectCycle(ListNode *head) {
+        if (!head) {
+            return NULL;
+        }
+     
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        // 1. 找到相遇点
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+         
+            if (slow == fast) {
+                break;
+            }          
+        }
+     
+        if (!fast || !fast->next) {
+            return NULL;
+        }
+     
+        // 2. slow 指向起始点
+        slow = head;
+     
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        return slow;
     }
 };
