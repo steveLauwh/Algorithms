67d4dd4fde0de276f08198e3188059ef405906b2
Steve Lau <liuwenhong_steve@163.com>
2017-08-05 11:18:01 +0800

    判断单链表是否存在环以及求出环的入口点

---
+++ Linked List/Linked List Cycle/LinkedListCycle.cpp
@@ -0,0 +1,22 @@
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ * Follow up:
+ * Can you solve it without using extra space?
+ * 判断单链表是否存在环以及求出环的入口点
+ */
+ 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+  
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+      
+    }
+};
