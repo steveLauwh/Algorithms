7469810682d73bb9ec897605a60c2af50d7f706f
Steve Lau <liuwenhong_steve@163.com>
2017-08-05 18:17:53 +0800

    不用加减运算符实现加法

--- Bit Manipulation/SumOfTwoIntegers/SumOfTwoIntegers.cpp
+++ Bit Manipulation/SumOfTwoIntegers/SumOfTwoIntegers.cpp
@@ -1,10 +1,17 @@
 /**
  * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
  */
-
+// 内含迭代思想
 class Solution {
 public:
     int getSum(int a, int b) {
-    
+        while (b) {
+            int carray = a & b;
+            int sum = a ^ b;
+            a = sum;
+            b = carray << 1;
+        }
+        
+        return a;
     }
 };
