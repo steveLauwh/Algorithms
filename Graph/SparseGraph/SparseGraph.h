35e4f8a4b0fde5ad7e60d3ff27979673a5464c53
Steve Lau <liuwenhong_steve@163.com>
2017-08-20 23:32:21 +0800

    Update SparseGraph.h

--- Graph/SparseGraph/SparseGraph.h
+++ Graph/SparseGraph/SparseGraph.h
@@ -62,6 +62,18 @@ public:
 
         return false;
     }
+    
+    void show() {
+        for(int i = 0; i < n; i ++) {
+            cout << "vertex " << i << ":\t";
+
+            for(int j = 0; j < g[i].size(); j ++)
+                cout << g[i][j] << "\t";
+
+            cout << endl;
+        }
+    }
+
 
     // 遍历邻边——迭代器
     class sparseGraphIterator {
