776932387d3e6675a1a074f772420f3838355375
Steve Lau <liuwenhong_steve@163.com>
2017-08-20 19:52:16 +0800

    Update SparseGraph.h

--- Graph/SparseGraph/SparseGraph.h
+++ Graph/SparseGraph/SparseGraph.h
@@ -5,7 +5,7 @@
 #include <cassert>
 #include <vector>
 
-using namespce std;
+using namespace std;
 
 // 稀疏图——用邻接表表示
 class SparseGraph {
@@ -18,88 +18,88 @@ private:
 public:
     SparseGraph(int n, bool directed) {
         this->n = n;
-        thid->m = 0;
+        this->m = 0;
         this->directed = directed;
-        
+
         for (int i = 0; i < n; i++) {
             g.push_back(vector<int>()); // 初始每行都为空
         }
     }
-    
+
     ~SparseGraph() {
-    
+
     }
-    
+
     int V() {
         return n;  // 顶点数
     }
-    
+
     int E() {
         return m;  // 边数
     }
-    
+
     void addEdge(int v, int w) {
         assert(v >= 0 && v < n);
         assert(w >= 0 && w < n);
-        
+
         g[v].push_back(w);
-        
+
         if (v != w && !directed) {  // 无向图
             g[w].push_back(v);
         }
-        
+
         m++;
     }
-    
+
     bool hasEdge(int v, int w) {
         assert(v >= 0 && v < n);
         assert(w >= 0 && w < n);
-        
+
         for (int i = 0; i < g[v].size(); i++) {
             if (g[v][i] == w)
                 return true;
         }
-        
+
         return false;
     }
-    
+
     // 遍历邻边——迭代器
     class sparseGraphIterator {
     private:
-        SpareseGraph &G;
+        SparseGraph &G;
         int v;
         int index;
-        
+
     public:
-        spareseGraphIterator(SpareseGraph &graph, int v) : G(graph) {
-        `   this->v = v;
+        sparseGraphIterator(SparseGraph &graph, int v) : G(graph) {
+            this->v = v;
             this->index = 0;
         }
-        
+
         int begin() {
             index = 0;
-            if (G.[v].size()) {
-                return G.[v][index];
+            if (G.g[v].size()) {
+                return G.g[v][index];
             }
-            
+
             return -1;
         }
-        
+
         int next() {
             index++;
-            if (index < G.[v].size()) {
-                return G.[v][index];
+            if (index < G.g[v].size()) {
+                return G.g[v][index];
             }
-            
+
             return -1;
         }
-        
+
         bool end() {
-            return index >= G.[v].size();
+            return index >= G.g[v].size();
         }
     };
-    
-    
+
+
 };
 
 #endif
