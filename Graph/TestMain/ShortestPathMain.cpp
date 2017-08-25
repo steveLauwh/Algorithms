497a58e8c74c68ce7fe04e5c3af82edc66bfb050
Steve Lau <liuwenhong_steve@163.com>
2017-08-25 11:32:20 +0800

    Create ShortestPathMain.cpp

---
+++ Graph/TestMain/ShortestPathMain.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "DenseGraph.h"
+#include "SparseGraph.h"
+#include "FindPath.h"
+#include "ReadGraph.h"
+#include "ShortestPath.h"
+
+using namespace std;
+
+int main() {
+    string filename = "test.txt";
+    
+    SparseGraph g(10, false);
+    
+    ReadGraph<SparseGraph> readGraph(g, filename);
+    
+    g.show();
+    
+    cout << endl;
+    
+    // 比较使用深度优先遍历和广度优先遍历获得路径的不同
+    // 广度优先遍历获得的是无权图的最短路径
+    FindPath<SparseGraph> dfs(g, 0);
+    cout<<"DFS : ";
+    dfs.showPath(6);
+
+    ShortestPath<SparseGraph> bfs(g,0);
+    cout<<"BFS : ";
+    bfs.showPath(6);
+    
+    return 0;
+}
