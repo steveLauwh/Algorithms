aeefd750f7e9fe5ca51b5fece5cfa6b15a868325
Steve Lau <liuwenhong_steve@163.com>
2017-08-29 09:31:08 +0800

    Update MinStack.cpp

--- Stack/Min Stack/MinStack.cpp
+++ Stack/Min Stack/MinStack.cpp
@@ -6,20 +6,31 @@ public:
     }
     
     void push(int x) {
+        st.push(x);
         
+        if (minst.empty() || x <= minst.top()) {
+            minst.push(x);
+        }
     }
     
     void pop() {
+        if (st.top() == minst.top()) {
+            minst.pop();
+        }
         
+        st.pop();
     }
     
     int top() {
-        
+        return minst.top();
     }
     
     int getMin() {
-        
+        return minst.top();
     }
+    
+private:
+    stack<int> st, minst;
 };
 
 /**
