6f0aed36592fafc90893221b74cd473a45e7cefa
Steve Lau <liuwenhong_steve@163.com>
2017-09-08 13:53:16 +0800

    读取图

--- Graph/ReadGraph/ReadGraph.h
+++ Graph/ReadGraph/ReadGraph.h
@@ -12,31 +12,31 @@ using namespace std;
 template <typename Graph>
 class ReadGraph {
 public:
-	ReadGraph(Graph &graph, const string &filename) {
-		ifstream file(filename);
-		string line;
-		int V, E;
+    ReadGraph(Graph &graph, const string &filename) {
+	ifstream file(filename);
+	string line;
+	int V, E;
 
-		assert(file.is_open());
-		assert(getline(file, line)); // 文件第一行，V 和 E
+	assert(file.is_open());
+	assert(getline(file, line)); // 文件第一行，V 和 E
+	stringstream s(line);
+	s >> V >> E;
+
+	assert(V == graph.V());
+
+	for (int i = 0; i < E; i++) {
+		assert(getline(file, line));
 		stringstream s(line);
-		s >> V >> E;
 
-		assert(V == graph.V());
+		int a, b;
 
-		for (int i = 0; i < E; i++) {
-			assert(getline(file, line));
-			stringstream s(line);
+		s >> a >> b;
+		assert(a >= 0 && a < V);
+		assert(b >= 0 && b < V);
 
-			int a, b;
-
-			s >> a >> b;
-			assert(a >= 0 && a < V);
-			assert(b >= 0 && b < V);
-
-			graph.addEdge(a, b);
-		}
+		graph.addEdge(a, b);
 	}
+    }
 };
 
 #endif /* READGRAPH_H_ */
