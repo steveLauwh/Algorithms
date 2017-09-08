9db02a6fd244831455cfbcdf9554f283c5e102f2
Steve Lau <liuwenhong_steve@163.com>
2017-09-08 14:40:53 +0800

    Update main.cpp

--- Graph/Minimum Span Tree/Weighted Graph/main.cpp
+++ Graph/Minimum Span Tree/Weighted Graph/main.cpp
@@ -13,14 +13,20 @@ int main() {
     
     // Test Weighted Dense Graph，无向图
     WeightedDenseGraph<double> g1 = WeightedDenseGraph<double>(V, false);
+    
     ReadWeightedGraph<WeightedDenseGraph<double>, double> readGraph1(g1, filename);
+    
     g1.show();
+    
     cout<<endl;
 
     // Test Weighted Sparse Graph
     WeightedSparseGraph<double> g2 = WeightedSparseGraph<double>(V, false);
-    ReadWeightedGraph<ReadWeightedGraph<double>, double> readGraph2(g2, filename);
+    
+    ReadWeightedGraph<WeightedSparseGraph<double>, double> readGraph2(g2, filename);
+    
     g2.show();
+    
     cout<<endl;
 
     return 0;
