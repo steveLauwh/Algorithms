1bf8abb9b80b4703255efe32d1408e6c48328c50
Steve Lau <liuwenhong_steve@163.com>
2017-09-21 11:27:36 +0800

    Create MoveZeroes.cpp

---
+++ Array/Move Zeroes/MoveZeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i]) {
+                nums[j++] = nums[i];
+            }            
+        }
+        
+        for (; j < nums.size(); j++) {
+            nums[j] = 0;
+        }
+    }
+};
