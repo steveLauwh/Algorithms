a1f83e384d11523ca3e98acfe8786db6cfb99b39
Steve Lau <liuwenhong_steve@163.com>
2017-09-23 13:43:35 +0800

    Update UnionFind.h

--- Graph/Minimum Span Tree/Kruskal/UnionFind.h
+++ Graph/Minimum Span Tree/Kruskal/UnionFind.h
@@ -9,66 +9,66 @@ using namespace std;
 // Quick Union + rank + path compression
 class UnionFind {
 public:
-	UnionFind(int count) {
-		parent = new int[count];
-		rank = new int[count];
-		this->count = count;
+    // 构造函数
+    UnionFind(int count) {
+	parent = new int[count];
+	rank = new int[count];
+	this->count = count;
 
-		for (int i = 0; i < count; i++) {
-			parent[i] = i;
-			rank[i] = 1;
-		}
+	for (int i = 0; i < count; i++) {
+	    parent[i] = i;
+	    rank[i] = 1;
+	}
+    }
+
+    ~UnionFind() {
+	delete[] parent;
+	delete[] rank;
+    }
+    
+    // 查找过程，O(h), h 为树的高度
+    int find(int p) {
+	assert(p >= 0 && p < count);
+
+	// path compression
+	while (p != parent[p]) {
+	    parent[p] = parent[parent[p]];
+	    p = parent[p];
 	}
 
-	~UnionFind() {
-		delete[] parent;
-		delete[] rank;
-	}
+	return p;
+    }
 
-	// 查找过程，O(h), h 为树的高度
-	int find(int p) {
-		assert(p >= 0 && p < count);
+    // 查看元素 p 和 q 是否所属一个集合
+    bool isConnected(int p, int q) {
+	return find(p) == find(q);
+    }
 
-		// path compression
-		while (p != parent[p]) {
-			parent[p] = parent[parent[p]];
-			p = parent[p];
-		}
+    // 合并元素 p 和元素 q 所属的集合
+    void unionElements(int p, int q) {
+	int pRoot = find(p);
+	int qRoot = find(q);
 
-		return p;
-	}
+	if (pRoot == qRoot) {
+	    return;
+        }
 
-	// 查看元素 p 和 q 是否所属一个集合
-	bool isConnected(int p, int q) {
-		return find(p) == find(q);
-	}
-
-	// 合并元素 p 和元素 q 所属的集合
-	void unionElements(int p, int q) {
-		int pRoot = find(p);
-		int qRoot = find(q);
-
-		if (pRoot == qRoot) {
-			return;
-		}
-
-		// 根据两个元素所在树的元素个数不同判断合并方向
-		// 将元素个数少的集合合并到元素个数多的集合上
-		if (rank[pRoot] < rank[qRoot]) {
-			parent[pRoot] = qRoot;
-		} else if( rank[qRoot] < rank[pRoot]) {
-		    parent[qRoot] = pRoot;
-		} else { // rank[pRoot] == rank[qRoot]
-		    parent[pRoot] = qRoot;
-		    rank[qRoot] += 1;   // 此时, 我维护rank的值
-		}
-	}
+        // 根据两个元素所在树的元素个数不同判断合并方向
+    	// 将元素个数少的集合合并到元素个数多的集合上
+    	if (rank[pRoot] < rank[qRoot]) {
+	    parent[pRoot] = qRoot;
+    	} else if( rank[qRoot] < rank[pRoot]) {
+	    parent[qRoot] = pRoot;
+    	} else { // rank[pRoot] == rank[qRoot]
+	    parent[pRoot] = qRoot;
+	    rank[qRoot] += 1;   
+    	}
+    }
 
 private:
-	int *rank;	 // rank[i]表示以i为根的集合所表示的树的层数
-	int *parent; // parent[i]表示第i个元素所指向的父节点
-	int count;   // 数据个数
-
+    int *rank;	 // rank[i]表示以i为根的集合所表示的树的层数
+    int *parent; // parent[i]表示第i个元素所指向的父节点
+    int count;   // 数据个数
 };
 
 #endif /* UNIONFIND_H_ */
