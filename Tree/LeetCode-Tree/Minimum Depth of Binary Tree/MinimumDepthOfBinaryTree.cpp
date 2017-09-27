59d9d09176b6c6e3915e0f58531f93c413a29704
Steve Lau <liuwenhong_steve@163.com>
2017-09-27 10:10:45 +0800

    Create MinimumDepthOfBinaryTree.cpp

---
+++ Tree/LeetCode-Tree/Minimum Depth of Binary Tree/MinimumDepthOfBinaryTree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        
+        if (!root->left && !root->right) {
+            return 1;
+        }
+        
+        if (!root->left) {
+            return minDepth(root->right) + 1;
+        } else if (!root->right) {
+            return minDepth(root->left) + 1;
+        } else {
+            return min(minDepth(root->left), minDepth(root->right)) + 1;
+        }
+    }
+};
