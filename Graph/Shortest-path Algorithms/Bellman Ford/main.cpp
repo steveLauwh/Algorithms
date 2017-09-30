86a4f5ab13a3003b7bd6a9844e2e8c3602c7cac9
Steve Lau <liuwenhong_steve@163.com>
2017-09-30 11:23:37 +0800

    Create main.cpp

---
+++ Graph/Shortest-path Algorithms/Bellman Ford/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "WeightedSparseGraph.h"
+#include "ReadWeightedGraph.h"
+#include "BellmanFord.h"
+
+using namespace std;
+
+// 测试Bellman-Ford算法
+int main() {
+
+    string filename = "test.txt";
+    int V = 5;
+
+    // 有向图
+    SparseGraph<int> g = SparseGraph<int>(V, true);
+    ReadGraph<SparseGraph<int>, int> readGraph(g, filename);
+
+    cout<<"Test Bellman-Ford: "<<endl<<endl;
+    BellmanFord<SparseGraph<int>, int> bellmanFord(g, 0);
+    if( bellmanFord.negativeCycle() )
+        cout<<"The graph contain negative cycle!"<<endl;
+    else
+        for( int i = 1 ; i < V ; i ++ ) {
+            if (bellmanFord.hasPathTo(i)) {
+                cout << "Shortest Path to " << i << " : " << bellmanFord.shortestPathTo(i) << endl;
+                bellmanFord.showPath(i);
+            }
+            else
+                cout << "No Path to " << i << endl;
+
+            cout << "----------" << endl;
+        }
+
+    return 0;
+}
