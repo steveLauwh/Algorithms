14310cd583d7c947700df081478c1153e9ec9d3c
Steve Lau <stevelauwh@163.com>
2020-11-14 23:49:28 +0800

    Create 54. multiply.py

---
+++ sfo/54. multiply.py
@@ -0,0 +1,21 @@
+## 题目描述
+
+```
+给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],
+其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。
+（注意：规定B[0] = A[1] * A[2] * ... * A[n-1]，B[n-1] = A[0] * A[1] * ... * A[n-2];）
+对于A长度为1的情况，B无意义，故而无法构建，因此该情况不会存在。
+```
+
+```cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> multiply(const vector<int>& A) {
+    
+    }
+};
+```
